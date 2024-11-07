b9490ec43859023e3bf045f852cdfffd9dcb8dcb
gvidoou <gvidoou@gmail.com>
2024-11-07 16:14:32 +0200

    Fixed quantity data type

--- template.js
+++ template.js
@@ -134,7 +134,7 @@ function getContents(arr, platform) {
     
     let qt = 1;
     if (arr[i][keyQt])
-      qt = arr[i][keyQt];
+      qt = makeNumber(arr[i][keyQt]);
     
     if (platform === 'meta') {
       contents.push({
